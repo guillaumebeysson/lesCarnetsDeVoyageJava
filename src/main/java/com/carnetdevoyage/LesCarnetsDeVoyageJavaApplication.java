2012a349557e9ea1c2f791345b261109f32d7cb3
guillaumebeysson <guillaume.beysson@gmail.com>
2023-03-19 11:30:28 +0100

    first commit

---
+++ src/main/java/com/carnetdevoyage/LesCarnetsDeVoyageJavaApplication.java
@@ -0,0 +1,13 @@
+package com.carnetdevoyage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LesCarnetsDeVoyageJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LesCarnetsDeVoyageJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carnetdevoyage/LesCarnetsDeVoyageJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carnetdevoyage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LesCarnetsDeVoyageJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
